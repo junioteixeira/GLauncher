031a1a9542ca70a86fc5cd76de0b309b00829e43
Metalus <metal-away@live.com>
2014-01-11 22:23:47 -0200

    Alteração nas classes de Settings
    Algumas mudanças na leitura de atributo e leitura de propriedade

--- GLModule/SettingsGame/SettingsAttributes.cs
+++ GLModule/SettingsGame/SettingsAttributes.cs
@@ -7,6 +7,15 @@ using Microsoft.Win32;
 namespace GLModule.SettingsGame
 {
     /// <summary>
+    /// Informa se a configuração de registro será salva em LocalMachine ou CurrentUser
+    /// </summary>
+    public enum TypeRegistryLocal
+    {
+        LocalMachine,
+        CurrentUser
+    };
+
+    /// <summary>
     /// Informa que certa propriedade não é uma configuração, ou seja, será ignorada pelo código
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
@@ -16,7 +25,32 @@ namespace GLModule.SettingsGame
     /// Informa que a propriedade será gravada no registro
     /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
-    public sealed class RegisterConfigAttribute : System.Attribute { };
+    public sealed class RegisterConfigAttribute : System.Attribute 
+    {
+        /// <summary>
+        /// Informa se o a configuração será salva em LocalMachine ou CurrentUser
+        /// </summary>
+        public TypeRegistryLocal RegistryLocal { get; private set; }
+
+        /// <summary>
+        /// Local onde será salva a chave
+        /// </summary>
+        public string Path { get; private set; }
+
+        public RegisterConfigAttribute()
+        { }
+
+        /// <summary>
+        /// Informa que a propriedade será gravada no registro
+        /// </summary>
+        /// <param name="RegistryLocal">Informa se o a configuração será salva em LocalMachine ou CurrentUser</param>
+        /// <param name="Path">Local onde será salva a chave</param>
+        public RegisterConfigAttribute(TypeRegistryLocal RegistryLocal, string Path)
+        {
+            this.RegistryLocal = RegistryLocal;
+            this.Path = Path;
+        }
+    };
 
     /// <summary>
     /// Informa que a propriedade será gravada no arquivo externo
@@ -31,7 +65,8 @@ namespace GLModule.SettingsGame
         public string ExternFileBaseSerialization { get; set; }
 
         /// <summary>
-        /// 
+        /// Local onde será salvo no arquivo externo
+        /// Caso não seja específicado, será utilizado a variável da classe SettingsGameConstants
         /// </summary>
         public string Path { get; set; }
     }
--- GLModule/SettingsGame/SettingsGame.cs
+++ GLModule/SettingsGame/SettingsGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text;
+using System.IO;
 using GLModule.Constants;
 using Microsoft.Win32;
 
@@ -10,27 +11,28 @@ namespace GLModule.SettingsGame
 {
     public static class Settings
     {
-       // [AnotherPath(AnotherPathEnum.CurrrentUser, "Software\\GameGLauncher\\Settings\\Graphic")]
+        [RegisterConfig]
         public static int GraphicAdapterIndex { get; set; }
 
         [RegisterConfig]
         public static int ResolutionIndex { get; set; }
 
-
+        [RegisterConfig]
         public static int AntialiasingIndex { get; set; }
 
-
+        [RegisterConfig]
         public static int AnisotropicFilteringIndex { get; set; }
 
-
+        [RegisterConfig]
         public static int Music { get; set; }
 
-
+        [RegisterConfig]
         public static int Sound { get; set; }
 
-
+        [RegisterConfig]
         public static bool WindowMode { get; set; }
 
+        [RegisterConfig]
         public static bool TopMost { get; set; }
 
         /// <summary>
@@ -43,6 +45,7 @@ namespace GLModule.SettingsGame
             try
             {
                 PropertyInfo[] propertys = SettingsGameConstants.TypeSettingsClass.GetProperties();
+                Dictionary<string, string> ExternFile = new Dictionary<string, string>();
                 for (int i = 0; i < propertys.Length; i++)
                 {
                     foreach (object att in propertys[i].GetCustomAttributes(false))
@@ -51,13 +54,55 @@ namespace GLModule.SettingsGame
                             goto ContinueFor;
                         else if (att is ExternFileConfigAttribute)
                         {
-                           ExternFileConfigAttribute externFile = att as ExternFileConfigAttribute;
-                       
+                            ExternFileConfigAttribute EF = att as ExternFileConfigAttribute;
+                            string Path = EF.Path.Length > 0 ? EF.Path : SettingsGameConstants.PathExternFile;
+                            string SerializationBase;
+                            if (!ExternFile.ContainsKey(Path))
+                            {
+                                SerializationBase = EF.ExternFileBaseSerialization.Length > 0 ? EF.ExternFileBaseSerialization : SettingsGameConstants.ExternFileBaseSerialization;
+                                SerializationBase = SerializationBase.Replace("[" + propertys[i].Name + "]", propertys[i].GetValue(null, null).ToString());
+                                ExternFile.Add(Path, SerializationBase);
+                            }
+                            else
+                                ExternFile[Path] = ExternFile[Path].Replace("[" + propertys[i].Name + "]", propertys[i].GetValue(null, null).ToString());
+                        }
+
+                        else if (att is RegisterConfigAttribute)
+                        {
+                            RegisterConfigAttribute RC = att as RegisterConfigAttribute;
+                            RegistryKey registry;
+                            if (RC.Path != String.Empty)
+                            {
+                                registry = RC.RegistryLocal == TypeRegistryLocal.CurrentUser ?
+                                                 Registry.CurrentUser.CreateSubKey(RC.Path) :
+                                                 Registry.LocalMachine.CreateSubKey(RC.Path);
+                            }
+                            else
+                                registry = SettingsGameConstants.PathRegistryKey;
+
+                            registry.SetValue(propertys[i].Name, propertys[i].GetValue(null, null));
+                        }
+
+                        else if (att is ParamsConfigAttribute)
+                        {
+                            string Param = "[" + propertys[i].Name + "]";
+                            if (SettingsGameConstants.ParamStartGame.IndexOf(Param) == -1)
+                                throw new Exception("Atributo " + Param + " não encontrado nos parâmetros do Jogo");
+                            SettingsGameConstants.ParamStartGame = SettingsGameConstants.ParamStartGame.Replace(Param, propertys[i].GetValue(null, null).ToString());
                         }
                     }
                 ContinueFor:
                     continue;
                 }
+
+                if (ExternFile.Count > 0)
+                    foreach (string Path in ExternFile.Keys)
+                    {
+                        if (File.Exists(Path)) { File.WriteAllText(Path, String.Empty); }
+                        using(FileStream FS = File.Open(Path,FileMode.OpenOrCreate))
+                            FS.Write(Encoding.ASCII.GetBytes(ExternFile[Path]),0,ExternFile[Path].Length);
+                    }
+
                 MessageErro = String.Empty;
                 return true;
             }
