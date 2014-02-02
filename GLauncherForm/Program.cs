134cea6165f646e63f673ef68a4f8973a59d9459
Metalus <metal-away@live.com>
2014-02-02 12:25:35 -0200

    Update Program.cs

--- GLauncherForm/Program.cs
+++ GLauncherForm/Program.cs
@@ -19,35 +19,5 @@ namespace GLauncherForm
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SplashForm());
         }
-
-        /// <summary>
-        /// Finds the MAC address of the NIC with maximum speed.
-        /// </summary>
-        /// <returns>The MAC address.</returns>
-        private static string GetMacAddress()
-        {
-            const int MIN_MAC_ADDR_LENGTH = 12;
-            string macAddress = string.Empty;
-            long maxSpeed = -1;
-
-            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                Console.WriteLine(
-                    "Found MAC Address: " + nic.GetPhysicalAddress() +
-                    " Type: " + nic.NetworkInterfaceType);
-
-                string tempMac = nic.GetPhysicalAddress().ToString();
-                if (nic.Speed > maxSpeed &&
-                    !string.IsNullOrEmpty(tempMac) &&
-                    tempMac.Length >= MIN_MAC_ADDR_LENGTH)
-                {
-                    Console.WriteLine("New Max Speed = " + nic.Speed + ", MAC: " + tempMac);
-                    maxSpeed = nic.Speed;
-                    macAddress = tempMac;
-                }
-            }
-
-            return macAddress;
-        }
     }
 }
